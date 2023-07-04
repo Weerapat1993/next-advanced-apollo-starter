aa624e68a4c5373418ce02da8725d4684d55ea92
Top-Weerapat-Mungmee <137028616+Top-Weerapat-Mungmee@users.noreply.github.com>
2023-07-05 00:11:37 +0700

    feat(NEX-7): Add Line Webhooks (#12)

---
+++ netlify/functions/api.ts
@@ -0,0 +1,87 @@
+import 'dotenv/config'
+import serverless from 'serverless-http';
+// Import all dependencies, mostly using destructuring for better view.
+import { ClientConfig, Client, middleware, MiddlewareConfig, WebhookEvent, TextMessage, MessageAPIResponseBase } from '@line/bot-sdk';
+import express, { Application, Request, Response, Router } from 'express';
+
+// Setup all LINE client and Express configurations.
+const clientConfig: ClientConfig = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || '',
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+const middlewareConfig: MiddlewareConfig = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET || '',
+};
+import packageJson from '../../package.json'
+
+// Create a new LINE SDK client.
+const client = new Client(clientConfig);
+
+// Create a new Express application.
+const api: Application = express();
+
+// Function handler to receive the text.
+const textEventHandler = async (event: WebhookEvent): Promise<MessageAPIResponseBase | undefined> => {
+  // Process all variables here.
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return;
+  }
+
+  // Process all message related variables here.
+  const { replyToken } = event;
+  const { text } = event.message;
+
+  // Create a new message.
+  const response: TextMessage = {
+    type: 'text',
+    text,
+  };
+
+  // Reply to the user.
+  await client.replyMessage(replyToken, response);
+};
+
+const router = Router();
+
+router.get('/version', (req, res) => res.status(200).json({
+  version: packageJson.version
+}));
+
+router.post(
+  '/webhook',
+  middleware(middlewareConfig),
+  async (req: Request, res: Response): Promise<Response> => {
+    const events: WebhookEvent[] = req.body.events;
+
+    // Process all of the received events asynchronously.
+    const results = await Promise.all(
+      events.map(async (event: WebhookEvent) => {
+        try {
+          await textEventHandler(event);
+        } catch (err: unknown) {
+          if (err instanceof Error) {
+            console.error(err);
+          }
+
+          // Return an error message.
+          return res.status(500).json({
+            status: 'error',
+          });
+        }
+      })
+    );
+
+    // Return a successfull message.
+    return res.status(200).json({
+      status: 'success',
+      results,
+    });
+  }
+);
+
+
+api.use('/api/', router);
+
+export const handler = serverless(api);
